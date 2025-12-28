81e322eb26a9da42bfc9813944be342bc9a9dd2b
Apoorv Mittal <apoorvmitthu@gmail.com>
2025-12-28 17:34:54 +0530

    Adding the national bird of India

---
+++ src/main/java/com/opencode/alumxbackend/Animal/Peacock.java
@@ -0,0 +1,16 @@
+package com.opencode.alumxbackend.Animal;
+
+public class Peacock {
+    
+    public String makeSound() {
+        return "Squawk!";
+    }
+
+    public String habitat() {
+        return "Forest";
+    }
+
+    public int numberOfLegs() {
+        return 2;
+    }
+}
