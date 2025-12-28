b354f9bd1cf4c33589280b5ebead7c4b7fb9694b
Ishant89op <ishant898734@gmail.com>
2025-12-28 17:53:32 +0530

    Cow added

---
+++ src/main/java/com/opencode/alumxbackend/Animal/Cow.java
@@ -0,0 +1,55 @@
+package com.opencode.alumxbackend.Animal;
+
+public class Cow {
+    public String whatIsYourName() {
+        return "Moo!";
+    }
+
+    public String whatIsYourFathersName() {
+        return "Moo!";
+    }
+
+    public String whatIsYourMothersName() {
+        return "Moo!";
+    }
+
+    public String whatIsYourFavouriteFood() {
+        return "Moo!";
+    }
+
+    public int numberOfLegs() {
+        return 4;
+    }
+
+    public String haveHorns() {
+        return "Yes";
+    }
+
+    public String isUseful() {
+        return "Yes";
+    }
+
+    public String byproduct() {
+        return "Milk";
+    }
+
+    public String canBeEaten() {
+        return "Yes";
+    }
+
+    public String isHerbivore() {
+        return "Yes";
+    }
+
+    public String isCarnivore() {
+        return "No";
+    }
+
+    public String isOmnivore() {
+        return "No";
+    }
+
+    public String favouriteFood() {
+        return "Grass";
+    }
+}
